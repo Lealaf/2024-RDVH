267d5c9648a3dab8926a5124c9eba187345f43cd
DawnFJ <florent.jamin@gmail.com>
2024-09-28 22:37:36 +0200

    Update materials

--- Assets/02_Scripts/UI/DisplayedObject.cs
+++ Assets/02_Scripts/UI/DisplayedObject.cs
@@ -4,12 +4,11 @@ public class ManageObjectClick : MonoBehaviour
     [SerializeField]
     public CheckObjectMenu checkObjectMenu;
 
-    private Material mat;
+    private Material[] mat;
     void Start()
     {
         checkObjectMenu.ReportEvent.AddListener(HideMeMaybe);
-        mat = gameObject.GetComponent<MeshRenderer>().material;
-        Debug.Log("mat = "+ mat+"  "+ gameObject.GetComponent<MeshRenderer>());
+        mat = gameObject.GetComponentInChildren<MeshRenderer>().materials;
     }
     void OnMouseDown()
     {
@@ -18,13 +17,19 @@ public class ManageObjectClick : MonoBehaviour
 
     private void OnMouseOver()
     {
-        mat.SetInt("_hover", 1);
+        Debug.Log(" => " + mat.Length);
+        for(int i = 0; i < mat.Length; i++)
+        {
+            mat[i].SetInt("_hover", 1);
+        }
     }
 
     private void OnMouseExit()
     {
-
-        mat.SetInt("_hover", 0);
+        for (int i = 0; i < mat.Length; i++)
+        {
+            mat[i].SetInt("_hover", 0);
+        }
     }
 
     public void HideMeMaybe(string id)
