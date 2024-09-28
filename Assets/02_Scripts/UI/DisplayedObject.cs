837029e23debca8a487ef85b7a8a0e33adfb1740
DawnFJ <florent.jamin@gmail.com>
2024-09-28 20:39:39 +0200

    Add onHover for objects

--- Assets/02_Scripts/UI/DisplayedObject.cs
+++ Assets/02_Scripts/UI/DisplayedObject.cs
@@ -3,14 +3,30 @@ public class ManageObjectClick : MonoBehaviour
 {
     [SerializeField]
     public CheckObjectMenu checkObjectMenu;
+
+    private Material mat;
     void Start()
     {
         checkObjectMenu.ReportEvent.AddListener(HideMeMaybe);
+        mat = gameObject.GetComponent<MeshRenderer>().material;
+        Debug.Log("mat = "+ mat+"  "+ gameObject.GetComponent<MeshRenderer>());
     }
     void OnMouseDown()
     {
         checkObjectMenu.HydrateAndShow(name);
     }
+
+    private void OnMouseOver()
+    {
+        mat.SetInt("_hover", 1);
+    }
+
+    private void OnMouseExit()
+    {
+
+        mat.SetInt("_hover", 0);
+    }
+
     public void HideMeMaybe(string id)
     {
         if (id == name) {
