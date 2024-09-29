f66e388fc14127fe56d37f6e266d2cd2c8e6c7d4
Nicolas Decoster <ogadaki@mailbox.org>
2024-09-29 14:14:33 +0200

    Add compute score and check if a given ID is anachronic

--- Assets/02_Scripts/Data/GameState.cs
+++ Assets/02_Scripts/Data/GameState.cs
@@ -12,8 +12,10 @@ public class GameState
 
     public static void Init()
     {
-        collected = new HashSet<string>();
-        EventManager.Instance.CollectedObjectsUpdated.Invoke();
+        if (collected == null) {
+            collected = new HashSet<string>();
+            EventManager.Instance.CollectedObjectsUpdated.Invoke();
+        }
     }
 
     public static void CollectObject(string id)
@@ -25,5 +27,47 @@ public class GameState
     public static int NumberOfCollectedObjects()
     {
         return collected == null ? 0 : collected.Count;
+    }
+
+    public static bool IsAnachronic(string id)
+    {
+        Debug.Log(id);
+        if (DataBase.data.objects.Exists(o => o.ID == id)) {
+            Debug.Log("found");
+            var obj = DataBase.data.objects.Find(o => o.ID == id);
+            Debug.Log(obj.type);
+            if (obj.type == "anachronique") {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            Debug.Log("not found");
+            return false;
+        }
+
+    }
+
+    public static int GetScore()
+    {
+        if (collected == null) {
+            return 0;
+        }
+        var all = 0;
+        var good = 0;
+        var bad = 0;
+        foreach(var id in collected)
+        {
+            all++;
+            if (IsAnachronic(id))
+                good++;
+            else
+                bad++;
+        }
+        Debug.Log("all = " + all);
+        Debug.Log("good = " + good);
+        Debug.Log("bad = " + bad);
+
+        return good - bad;
     }
 }
