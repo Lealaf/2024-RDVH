b985e22d877b34cf6222ce1cb2c5413d51c2296f
Léa L <47750546+Lealaf@users.noreply.github.com>
2024-10-06 20:36:58 +0200

    🚧 remove button change camera when check object

--- Assets/02_Scripts/UI/CheckObjectMenu.cs
+++ Assets/02_Scripts/UI/CheckObjectMenu.cs
@@ -9,9 +9,6 @@ public class CheckObjectMenu : MonoBehaviour
     [SerializeField]
     CanvasGroup canvasGroup;
 
-    [SerializeField]
-    DocumentInformationObject carnet;
-
 
 
     [SerializeField]
@@ -26,38 +23,26 @@ public class CheckObjectMenu : MonoBehaviour
     GameObject viewver;
 
 
-    void Start()
-    {
-        EventManager.Instance.SelectObject.AddListener(HydrateAndShow);
-    }
+    
 
-    //TODO passer en parametre la classe avec tt les info pour hydrater tt
-    public void HydrateAndShow(DisplayedObject obj)
+
+    public void Hydrate(DisplayedObject obj)
     {
         this.idObject = obj.objectName;
-        Show(true);
     }
 
 
     public void PutBack()
     {
-        Show(false);
         PutBackEvent?.Invoke();
         AudioManager.Instance.putDownObjectNoise();
     }
 
     public void Report()
     {
-        Show(false);
         GameState.CollectObject(idObject);
         EventManager.Instance.CollectObject.Invoke(idObject);
         AudioManager.Instance.takeObjectNoise();
-    }
-
-    public void Show(bool show)
-    {
-        canvasGroup.alpha = show?1:0;
-        canvasGroup.interactable = show;
     }
 
 
